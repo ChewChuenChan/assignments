3256bb891567dafa7ce748291c90fc535f2c9f0e
Chew Chuen Chan <aliceccc@gmail.com>
2022-04-27 12:44:31 -0400

    initial commit

--- java/Chew_Chan_Array/4_ArrayReverse.js
+++ java/Chew_Chan_Array/4_ArrayReverse.js
@@ -2,13 +2,12 @@
 // Write a function that will reverse that values an array and return them
 
 function reverse(arr) {
-var arr=[];
-var rev=[];
-for(var i= (arr.length+1); i <=0; i--){
-    
-}
-    return arr;
-
-}
- var result = reverse(["a","b","c","d","e"]);
- console.log(result);
+    var temp=[];
+    for(var i=(arr.length-1); i>=0; i--){
+    temp.push(arr[i]);
+    }
+        arr=temp;
+        return arr;
+    }
+     var result = reverse(["a","b","c","d","e"]);
+    console.log(result);
